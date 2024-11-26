5d3981d4a46510e2ed175ce09f633903025386bd
dalmook <chosungmook@naver.com>
2024-11-27 04:43:32 +0900

    Create logicgamefirebaseConfig.js

---
+++ logicgamefirebaseConfig.js
@@ -0,0 +1,15 @@
+// firebaseConfig.js
+
+// Firebase 구성 정보 (Firebase 콘솔에서 복사한 실제 설정으로 교체)
+const firebaseConfig = {
+    apiKey: "AIzaSyCM5kmJDA9zeBg3zmutWk2urV1g3GDHMjw",
+    authDomain: "logicgamedalbong.firebaseapp.com",
+    projectId: "logicgamedalbong",
+    storageBucket: "logicgamedalbong.firebasestorage.app",
+    messagingSenderId: "185772514625",
+    appId: "1:185772514625:web:a8b9c944e7cdea588255c0"
+};
+
+// Firebase 초기화
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
