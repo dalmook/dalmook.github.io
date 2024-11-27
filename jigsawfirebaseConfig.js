890e8dcd807a08c88bd0f6951ad1dc5ee1bc6207
dalmook <94118801+dalmook@users.noreply.github.com>
2024-11-27 20:57:58 +0900

    Create jigsawfirebaseConfig.js

---
+++ jigsawfirebaseConfig.js
@@ -0,0 +1,21 @@
+// logicgamefirebaseConfig.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Export db for use in other scripts
+export { db };
