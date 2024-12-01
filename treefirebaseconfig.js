cea21718ce8464b68c3d2d1be2e549b254fec11f
dalmook <94118801+dalmook@users.noreply.github.com>
2024-12-01 10:40:57 +0900

    Create treefirebaseconfig.js

---
+++ treefirebaseconfig.js
@@ -0,0 +1,15 @@
+// firebaseConfig.js
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
