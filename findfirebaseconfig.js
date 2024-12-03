fb95d364319a2a8a90eb97a9b4e2fbd21c0cd0ee
dalmook <chosungmook@naver.com>
2024-12-03 21:29:40 +0900

    Update findfirebaseconfig.js

--- findfirebaseconfig.js
+++ findfirebaseconfig.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyCwVxx0Pxd7poc_zGSp1aY9qfd89bpVUW0",
     authDomain: "finddalbong.firebaseapp.com",
     projectId: "finddalbong",
-    storageBucket: "finddalbong.appspot.com", // 올바른 형식으로 수정
+    storageBucket: "finddalbong.firebasestorage.app", // 올바른 형식으로 수정
     messagingSenderId: "982765399272",
     appId: "1:982765399272:web:02344ab408272c60e2ad5d"
 };
